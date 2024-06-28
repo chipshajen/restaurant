b735000f168ca96f06936168a461ae77fda42015
chipshajen <sebastianwidlund@hotmail.com>
2024-06-28 03:50:50 -0500

    First module loaded

---
+++ src/pageLoad.js
@@ -0,0 +1,19 @@
+function loadPage(){
+    const element = document.querySelector('#content')
+
+    const img = new Image()
+    img.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmDrniCUtTuYTuWSDri6cLmcNsZROeiJcBzQ&s'
+
+    const h1 = document.createElement('h1')
+    h1.textContent = 'Very nice Restaurant'
+
+    const p = document.createElement('p')
+    p.textContent = 'This place is really nice, eat here'
+
+    element.appendChild(img)
+    element.appendChild(h1)
+    element.appendChild(p)
+}
+
+export default loadPage
+
